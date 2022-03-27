5146c59176b5f36b23d22e7e08dea4663cf3dc16
Piero <matematicadivina1@gmail.com>
2022-03-27 01:21:48 -0700

    conflict resolved of password

--- src/services/conexion.js
+++ src/services/conexion.js
@@ -7,5 +7,5 @@ import mongoose from 'mongoose'
 const ACCESDB = process.env
 
 export const main = () => {
-  return mongoose.connect(`mongodb://${ACCESDB.USER_NAME}:${ACCESDB.PASSWORD}@${ACCESDB.HOST}:27017/${ACCESDB.DATA_BASE_NAME}`);
+  return mongoose.connect(`mongodb://${ACCESDB.USER_NAME}:${ACCESDB.DATA_BASE_PASSWORD}@${ACCESDB.HOST}:27017/${ACCESDB.DATA_BASE_NAME}`);
 }
